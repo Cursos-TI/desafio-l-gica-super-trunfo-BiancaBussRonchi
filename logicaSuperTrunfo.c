1bdf487218e930fa0b8586912f8bf3b7bc724904
BiancaBussRonchi <158002753+BiancaBussRonchi@users.noreply.github.com>
2025-10-27 22:32:50 +0000

    Desafio logicaSuperTrunfo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,43 +1,91 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
+    // Dados das cidades
+    char nomeA[] = "Rio de Janeiro";
+    char nomeB[] = "Sao Paulo";
 
-    
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+    int populacaoA = 6748000;
+    int populacaoB = 12330000;
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+    float areaA = 1200.27;
+    float areaB = 1521.11;
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+    float pibA = 407.0;   // em bilhões
+    float pibB = 699.0;   // em bilhões
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
+    int pontosTuristicosA = 25;
+    int pontosTuristicosB = 40;
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
+    // Cálculos automáticos
+    float densidadeA = populacaoA / areaA;
+    float densidadeB = populacaoB / areaB;
+    float pibPerCapitaA = (pibA * 1000000000) / populacaoA;
+    float pibPerCapitaB = (pibB * 1000000000) / populacaoB;
 
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+    int vitoriasA = 0, vitoriasB = 0;
+
+    printf("=== Comparacao Super Trunfo: %s x %s ===\n\n", nomeA, nomeB);
+
+    // População
+    printf("Populacao: ");
+    if (populacaoA > populacaoB) {
+        printf("%s vence\n", nomeA); vitoriasA++;
+    } else {
+        printf("%s vence\n", nomeB); vitoriasB++;
+    }
+
+    // Área
+    printf("Area: ");
+    if (areaA > areaB) {
+        printf("%s vence\n", nomeA); vitoriasA++;
+    } else {
+        printf("%s vence\n", nomeB); vitoriasB++;
+    }
+
+    // PIB
+    printf("PIB: ");
+    if (pibA > pibB) {
+        printf("%s vence\n", nomeA); vitoriasA++;
+    } else {
+        printf("%s vence\n", nomeB); vitoriasB++;
+    }
+
+    // PIB per capita
+    printf("PIB per capita: ");
+    if (pibPerCapitaA > pibPerCapitaB) {
+        printf("%s vence\n", nomeA); vitoriasA++;
+    } else {
+        printf("%s vence\n", nomeB); vitoriasB++;
+    }
+
+    // Densidade demográfica (menor vence)
+    printf("Densidade demografica: ");
+    if (densidadeA < densidadeB) {
+        printf("%s vence\n", nomeA); vitoriasA++;
+    } else {
+        printf("%s vence\n", nomeB); vitoriasB++;
+    }
+
+    // Pontos turísticos
+    printf("Pontos turisticos: ");
+    if (pontosTuristicosA > pontosTuristicosB) {
+        printf("%s vence\n", nomeA); vitoriasA++;
+    } else {
+        printf("%s vence\n", nomeB); vitoriasB++;
+    }
+
+    // Resultado final
+    printf("\n=== Resultado Final ===\n");
+    printf("%s venceu %d atributos.\n", nomeA, vitoriasA);
+    printf("%s venceu %d atributos.\n", nomeB, vitoriasB);
+
+    if (vitoriasA > vitoriasB)
+        printf("\n Cidade vencedora: %s!\n", nomeA);
+    else if (vitoriasB > vitoriasA)
+        printf("\n Cidade vencedora: %s!\n", nomeB);
+    else
+        printf("\nEmpate entre as cidades!\n");
 
     return 0;
 }
